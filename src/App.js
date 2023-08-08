76a0d4d1f343c901a7af296d4742bf116689965a
Mumin Bashir <moominbashir732@gmail.com>
2023-08-08 22:43:17 +0530

    made navbar and feed component with sidebar and searchbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 const app = () => {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Feed />} />
+          <Route exact path="/video/:id" element={<VideoDetail />} />
+          <Route exact path="/channel/:id" element={<ChannelDetail />} />
+          <Route exact path="/search/:searchItem" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default app;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        backgroundColor: "#000",
+        top: "0",
+        justifyContent: "space-between",
+      }}
+    >
+      <Link
+        to="/"
+        style={{ display: "flex", alignItems: "center", color: "White" }}
+      >
+        <img src={logo} alt="logo" height={45} />
+        <span className="logo-text">BitTube</span>
+      </Link>
+      <SearchBar />
+    </Stack>
+  );
+};
+
+export default Navbar;
